ef7e72b14ea80910766af8f9fead65c8dbb2255d
Rahul Mitra <rahulm@bu.edu>
2022-11-30 22:56:39 -0500

    simple echo application

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
-
+from flask import Flask, request
 app = Flask(__name__)
-
-
-@app.route('/')
+ 
+@app.route("/")
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return '<form action="/echo" method="GET"><input name="text"><input type="submit" value="Echo"></form>'
+ 
+@app.route("/echo")
+def echo():
+    return "You said: " + request.args.get('text', '')
+ 
+ 
+if __name__ == "__main__":
+    app.run()
