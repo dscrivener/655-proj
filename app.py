de5a75f3e01f1134f5a715d2bd57b9cb30f856fd
gaabrielfranco <gabrielvsfranco@outlook.com>
2022-12-03 19:13:25 -0500

    Worker and App: socket communication

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, request
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
 app = Flask(__name__)
  
 @app.route("/")
 def hello():
-    return '<form action="/echo" method="GET"><input name="text"><input type="submit" value="Echo"></form>'
+    return '<form action="/echo" method="GET"><input name="text"><input type="submit" value="Submit"></form>'
  
 @app.route("/echo")
 def echo():
-    return "You said: " + request.args.get('text', '')
- 
- 
-if __name__ == "__main__":
-    app.run()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(request.args.get('text', '').encode("utf-8"))
+        match = s.recv(1024)
+        return '''
+        <h2>{}</h2>
+        <p align="left"><a href=/ ><button>Try another MD5 Hash</button></a></p>
+        '''.format(match.decode("utf-8"))
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000)
--- worker.py
+++ worker.py
@@ -1,16 +1,23 @@
-import sys
 import hashlib
 import itertools
+import socket
 
-# first command-line argument is interpreted as a hash to crack
-# anything else is ignored
-if len(sys.argv) > 1:
-    hash = sys.argv[1]
-    if len(hash) != 32:
-        print('ERROR: not a valid MD5 hash')
-        exit(-1)
-    # A - Z : 65 - 90
-    # a - z : 97 - 122
+# Too expensive because it try all possible combinations - 
+# have to find a way to finish the execution after finding the match
+
+# from joblib import Parallel, delayed
+# def parallel_crack_hash(hash, candidate):
+#     if hashlib.md5(bytearray(candidate)).hexdigest() == hash:
+#         # we found a match
+#         print(f'Match: {candidate}')
+#         return candidate
+
+# hash = "594f803b380a41396ed63dca39503542"
+# charseq = list(range(65, 91)) + list(range(97, 123))
+# results = Parallel(n_jobs=-1, verbose=10)(delayed(parallel_crack_hash)(hash, candidate) for candidate in itertools.product(charseq, repeat=5))
+# print(results)
+
+def crack_hash(hash):
     charseq = list(range(65, 91)) + list(range(97, 123))
 
     for p in itertools.product(charseq, repeat=5):
@@ -18,11 +25,32 @@ if len(sys.argv) > 1:
             # we found a match
             m = ''.join(map(chr, p))
             print(f'Match: {m}')
-            exit(0)
+            return m
 
     # no match
     print('ERROR: hash not in space of 5-character alphabetic passwords')
-    exit(-1)
-else:
-    print('Arguments: hash')
-    exit(-1)
+    return None
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    while True:
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                hash = conn.recv(1024) #receiving hash data
+                if not hash:
+                    break
+                if len(hash) != 32:
+                    print('ERROR: not a valid MD5 hash')
+                    break
+
+                match = crack_hash(hash.decode("utf-8")) #cracking hash
+                if match is None:
+                    conn.sendall("ERROR: hash not in space of 5-character alphabetic passwords".encode())
+                else:
+                    conn.sendall(match.encode())
