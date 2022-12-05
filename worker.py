1b8f4cab635928a319854055f4c9e5fa779da118
gaabrielfranco <gabrielvsfranco@outlook.com>
2022-12-05 12:48:07 -0500

    App and Worker multithread

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 import hashlib
 import itertools
 import socket
+import threading
 
 # Too expensive because it try all possible combinations - 
 # have to find a way to finish the execution after finding the match
@@ -22,35 +23,42 @@ def crack_hash(hash):
 
     for p in itertools.product(charseq, repeat=5):
         if hashlib.md5(bytearray(p)).hexdigest() == hash:
-            # we found a match
             m = ''.join(map(chr, p))
             print(f'Match: {m}')
             return m
 
-    # no match
     print('ERROR: hash not in space of 5-character alphabetic passwords')
     return None
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+HOST = "172.17.2.14"
+PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    while True:
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                hash = conn.recv(1024) #receiving hash data
-                if not hash:
-                    break
-                if len(hash) != 32:
-                    print('ERROR: not a valid MD5 hash')
-                    break
+class ClientThread(threading.Thread):
+    def __init__(self, client_address, client_socket):
+        threading.Thread.__init__(self)
+        self.client_socket = client_socket
+        self.client_address = client_address
 
-                match = crack_hash(hash.decode("utf-8")) #cracking hash
-                if match is None:
-                    conn.sendall("ERROR: hash not in space of 5-character alphabetic passwords".encode())
-                else:
-                    conn.sendall(match.encode())
+    def run(self):
+        while True:
+            print("Client address: {}".format(self.client_address))
+            hash = self.client_socket.recv(1024)
+            if not hash:
+                break
+            if len(hash) != 32:
+                self.client_socket.sendall("ERROR: not a valid MD5 hash".encode())
+                break
+            match = crack_hash(hash.decode("utf-8"))
+            if match is None:
+                self.client_socket.sendall("ERROR: hash not in space of 5-character alphabetic passwords".encode())
+            else:
+                self.client_socket.sendall(match.encode())
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((HOST, PORT))
+while True:
+    server.listen(5)
+    client_socket, client_address = server.accept()
+    new_thread = ClientThread(client_address, client_socket)
+    new_thread.start()
