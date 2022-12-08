4dd0ff1cbf9ead6948f3553b78cf77c11c9249c6
gaabrielfranco <gabrielvsfranco@outlook.com>
2022-12-07 23:27:31 -0500

    Changes in the scripts

--- InstallScripts/ConfigureWorker.sh
+++ InstallScripts/ConfigureWorker.sh
@@ -1,13 +1,15 @@
 #this is the script for the server
+sudo apt-get update
 sudo apt-get install zip unzip #donwload unzipper
-sudo apt update
 sudo apt-get install python3.6 #install python 3.6
 sudo apt-get -y install python3-pip #install python3-pip
-sudo pip3 install Flask #install flask 
-sudo pip3 install pandas
-sudo pip3 install Pillow
-sudo pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu --no-cache-dir #install torch
+sudo -H pip3 install Flask #install flask 
+sudo -H pip3 install pandas
+sudo -H pip3 install Pillow
+sudo -H pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu --no-cache-dir #install torch
+sudo -H pip3 install supervisor gunicorn
+supervisord -c worker.conf 
 
-#run the flask app
-cd ..
-python3 worker_img.py
+# #run the flask app
+# cd ..
+# python3 worker_img.py
