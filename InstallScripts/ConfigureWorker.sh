653951acc5519d5536543d66402c623c4e771b19
Rahul Mitra <rahulm@bu.edu>
2022-12-07 21:55:24 -0500

    add worker bash file

---
+++ InstallScripts/ConfigureWorker.sh
@@ -0,0 +1,13 @@
+#this is the script for the server
+sudo apt-get install zip unzip #donwload unzipper
+sudo apt update
+sudo apt-get install python3.6 #install python 3.6
+sudo apt-get -y install python3-pip #install python3-pip
+sudo pip3 install Flask #install flask 
+sudo pip3 install pandas
+sudo pip3 install Pillow
+sudo pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu --no-cache-dir #install torch
+
+#run the flask app
+cd ..
+python3 worker_img.py
